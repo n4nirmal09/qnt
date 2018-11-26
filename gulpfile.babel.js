1cf7e15b3930069af580c67720ea6ea0892b93a7
n4nirmal09 <n4nirmal09@gmail.com>
2018-11-26 11:25:31 +0530

    initial setup

---
+++ gulpfile.babel.js
@@ -0,0 +1,251 @@
+"use strict";
+
+import gulp from "gulp";
+import batch from "gulp-batch";
+import watch from "gulp-watch";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import gutil from "gulp-util";
+import source from "vinyl-source-stream";
+import buffer from "vinyl-buffer";
+import sass from "gulp-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import minifyCSS from "gulp-csso";
+import sourcemaps from "gulp-sourcemaps";
+import babel from "gulp-babel";
+import browserify from "browserify";
+import babelify from "babelify";
+import browserSync from "browser-sync";
+import env from "babel-preset-env";
+import uglify from "gulp-uglify";
+import filter from "gulp-filter";
+import changed from "gulp-changed";
+import spritesmith from "gulp.spritesmith"
+import clean from "gulp-rimraf";
+import purify from "gulp-purifycss";
+import merge from "merge-stream";
+import glob from "glob";
+import path from "path";
+import injectPartials from 'gulp-inject-partials';
+import streamqueue from 'streamqueue';
+import concat from 'gulp-concat';
+
+
+
+
+browserSync.create();
+
+// PATHS
+const dirs = {
+  src: "src",
+  dest: "dist"
+};
+const paths = {
+  css: {
+    source: `${dirs.src}/scss/**/*.scss`,
+    watch: `${dirs.src}/scss/**/*.scss`,
+    dest: `${dirs.dest}/css/`
+  },
+  js: {
+    source: `${dirs.src}/js/main.js`,
+    dest: `${dirs.dest}/js/`,
+    watch: `${dirs.src}/js/main.js`
+  },
+  htmls: {
+    source: `${dirs.src}/templates/*.html`,
+    dest: `${dirs.dest}/`,
+    watch: `${dirs.src}/templates/**/*.html`,
+  },
+  images: {
+    source: `${dirs.src}/img/**/*.{png,jpg,jpeg,gif,svg}`,
+    dest: `${dirs.dest}/img/`,
+    watch: `${dirs.src}/img/**/*.{png,jpg,jpeg,gif,svg}`,
+  },
+  fonts: {
+    source: `${dirs.src}/fonts/**/*.*`,
+    dest: `${dirs.dest}/fonts/`,
+    watch: `${dirs.src}/fonts/**/*.*`,
+  },
+  spriteImages: {
+    source: `${dirs.src}/img/sprite/**/*.png`,
+    dest: `${dirs.src}/img/`,
+    watch: `${dirs.src}/img/sprite/**/*.png`,
+  }
+};
+
+// Clean Dist Directory
+gulp.task("clean", [], () => {
+  console.log("Clean all files in dist folder");
+  return gulp.src("dist/*", { read: false }).pipe(clean());
+});
+
+const plugins = [autoprefixer({ browsers: ["last 10 version"] })];
+
+
+// SASS
+gulp.task("sass", () =>
+  gulp
+    .src(paths.css.source)
+    .pipe(
+      plumber({
+        errorHandler: notify.onError({
+          message: "<%= error.message %>",
+          title: "CSS preprocessing"
+        })
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(browserSync.stream({ match: "**/*.css" }))
+);
+
+// Js
+
+gulp.task('vendors-js', function() {
+   return streamqueue({ objectMode: true },
+    gulp.src(`${dirs.src}/js/vendors/modernizr.js`),
+    gulp.src(`${dirs.src}/js/vendors/jquery-3.3.1.js`),
+    gulp.src(`${dirs.src}/js/vendors/greensock/TweenMax.js`),
+    gulp.src(`${dirs.src}/js/vendors/greensock/plugins/DrawSVGPlugin.js`),
+    gulp.src(`${dirs.src}/js/vendors/greensock/plugins/MorphSVGPlugin.js`),
+    gulp.src(`${dirs.src}/js/vendors/scrollmagic/ScrollMagic.js`),
+    gulp.src(`${dirs.src}/js/vendors/scrollmagic/plugins/animation.gsap.js`),
+    gulp.src(`${dirs.src}/js/vendors/slick.js`),
+    gulp.src(`${dirs.src}/js/vendors/jquery.easeScroll.js`)
+
+       //gulp.src('dev/lib/modernizr.custom.min.js'), 
+       //gulp.src('dev/lib/jquery-2.1.1.min.js'),
+       //gulp.src('dev/lib/greensock/utils/Draggable.js'), 
+       //gulp.src('dev/lib/greensock/utils/SplitText.js'), 
+       //gulp.src('dev/lib/greensock/TweenMax.js'),
+       //gulp.src('dev/lib/greensock/plugins/DrawSVGPlugin.js'),   
+       //gulp.src('dev/lib/greensock/plugins/MorphSVGPlugin.js'),
+       //gulp.src('dev/lib/greensock/plugins/ScrollToPlugin.js'),
+       //gulp.src('dev/lib/scrollmagic/ScrollMagic.js'),
+       //gulp.src('dev/lib/scrollmagic/plugins/animation.gsap.js'),
+       //gulp.src('dev/lib/slick.js'),
+       //gulp.src('dev/lib/select2.js'),  
+       //gulp.src('dev/lib/moment.js'),
+       //gulp.src('dev/lib/bootstrap.js'),  
+       //gulp.src('dev/lib/bootstrap-datepicker.js'),
+       //gulp.src('dev/lib/select2.min.js'),
+       //gulp.src('dev/lib/app/components/components.js'),
+       //gulp.src('dev/lib/app/app.js'),
+       //gulp.src('dev/lib/calendar.js'),
+       
+   )
+   .pipe(concat('vendors.js'))
+   .pipe(uglify('vendors.js'))
+   .pipe(gulp.dest(`${dirs.dest}/js/`));
+});
+
+// ES6 Bundles generator
+gulp.task("main-js", () => {
+  const files = glob.sync("./src/js/main.js");
+  return merge(
+    files.map(file =>
+      browserify({
+        entries: file,
+        debug: true
+      })
+        .transform(babelify.configure({ presets: [env] }))
+        .bundle()
+        .on(
+          "error",
+          notify.onError({
+            message: "<%= error.message %>",
+            title: "JS compilation"
+          })
+        )
+        .pipe(source(`${path.basename(file, ".js")}.js`))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.js.dest))
+        .pipe(browserSync.stream())
+    )
+  );
+});
+// Html Partial renderer 
+gulp.task('ptl_render', function () {
+  return gulp.src(paths.htmls.source)
+           .pipe(plumber(function (error) {
+                gutil.log(error.message);
+                this.emit('end');
+            }))
+           .pipe(injectPartials({
+              removeTags: true
+           }))
+           .pipe(gulp.dest(paths.htmls.dest))
+           .pipe(browserSync.stream());
+});
+// Images
+gulp.task("images", () =>
+  gulp
+    .src(paths.images.source)
+    .pipe(gulp.dest(paths.images.dest))
+);
+
+// Statics
+gulp.task("static-files", () =>
+  gulp
+    .src(`${dirs.src}/static/**/*.{png,jpg,jpeg,gif,svg,js,json}`)
+    .pipe(gulp.dest(`${dirs.dest}/static/`))
+);
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src(paths.spriteImages.source).pipe(spritesmith({
+    imgName: '../img/sprite.png',
+    cssName: '../scss/_sprite.scss'
+  }));
+  return spriteData.pipe(gulp.dest(paths.spriteImages.dest));
+});
+
+// fonts
+gulp.task("fonts", () =>
+  gulp.src(paths.fonts.source).pipe(gulp.dest(paths.fonts.dest))
+);
+
+
+// Gulp main serve task
+gulp.task("serve", function(){
+	browserSync.init({
+    server: {
+      baseDir: "./dist"
+    },
+    open: true
+    // port: 8000
+  });
+
+  gulp.watch(paths.css.watch, ["sass"]);
+  gulp.watch(`${dirs.src}/js/vendors/**/*.js`, ["vendors-js"]);
+  gulp.watch(paths.js.watch, ["main-js"]);
+  gulp.watch(paths.spriteImages.watch, ["sprite"])
+  gulp.watch(paths.images.watch, ["images"]);
+  gulp.watch(paths.fonts.watch, ["fonts"]);
+  gulp.watch(`${dirs.src}/static/**/*.{png,jpg,jpeg,gif,svg,js,json}`, ["static-files"]);
+  gulp.watch(paths.htmls.watch, ["ptl_render"]);
+
+})
+
+// gulp main build task
+gulp.task('build', [
+  "ptl_render",
+  "sass",
+  "vendors-js",
+  "main-js",
+  "sprite",
+  "images",
+  "fonts",
+  "static-files"
+])
+
+// Gulp default task
+gulp.task("default", ["build", "serve"]);
+
---
+++ src/js/main.js
@@ -0,0 +1,229 @@
+import "./utilities/smooth-scroll" 
+
+(function($) {
+
+  "use strict";
+
+  var S, Qobj = {
+    _init() {
+      S = Qobj.settings
+      this.resizeListener()
+
+
+      this.scrollDierectionChecker()
+      this.scrollBarWidth()
+      this.menuClick()
+      this.mainBanner()
+      this.gallerySlider()
+      this.testimonial()
+      this.events()
+      if ($('#main-loader').length) {
+        this.mainLoaderAnimation()
+      }
+
+    },
+
+    settings: {
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight,
+      scrollClassTrigger: 0,
+      scrollBarWidth: 0
+    },
+
+    // Debounce function to optimize event listeners. we dont want it fire every time.
+    debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this,
+          args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      }
+    },
+
+    // Resize Function
+    resizeFunctions() {
+      S.windowWidth = window.innerWidth
+      S.windowHeight = window.innerHeight
+      // console.log(S.windowHeight)
+    },
+
+    // ResizeListener 
+    resizeListener() {
+      let resize = this.debounce(this.resizeFunctions, 250)
+      window.addEventListener('resize', resize)
+    },
+
+    
+    //Update scroll bar width
+    scrollBarWidth() {
+      var outer = document.createElement("div");
+      outer.style.visibility = "hidden";
+      outer.style.width = "100px";
+      outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+
+      document.body.appendChild(outer);
+
+      var widthNoScroll = outer.offsetWidth;
+      // force scrollbars
+      outer.style.overflow = "scroll";
+
+      // add innerdiv
+      var inner = document.createElement("div");
+      inner.style.width = "100%";
+      outer.appendChild(inner);
+
+      var widthWithScroll = inner.offsetWidth;
+
+      // remove divs
+      outer.parentNode.removeChild(outer);
+
+      S.scrollBarWidth = widthNoScroll - widthWithScroll;
+
+
+    },
+    // Main menu toggler
+    menuClick() {
+      $(".mobile-toggle-button").on("click", function() {
+        $(this).toggleClass("act-menu")
+        $("body").toggleClass("show-menu")
+      })
+    },
+
+
+    // FUNCTIONS
+    mainBanner() {
+      $('.main-banner .slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+      })
+    },
+
+    gallerySlider() {
+      $('.gallery .slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // asNavFor: '.slider-for',
+        // dots: true,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [{
+          breakpoint: 1025,
+          settings: {
+            initialSlide: 0,
+            centerMode: true,
+            centerPadding: '20%',
+            slidesToShow: 1
+          }
+        }]
+      })
+    },
+
+    events() {
+      $('.events-slides .slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // asNavFor: '.slider-for',
+        // dots: true,
+        arrows: false,
+        // centerMode: true,
+        focusOnSelect: true,
+        responsive: [{
+          breakpoint: 1025,
+          settings: {
+            initialSlide: 0,
+            centerMode: true,
+            centerPadding: '20%',
+            slidesToShow: 1
+          }
+        }]
+      })
+    },
+
+    testimonial() {
+      $('.testimonials .slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+      })
+    },
+
+    // main loader animation
+    mainLoaderAnimation() {
+      let mainLoader = $('#main-loader'),
+        tl = new TimelineMax({
+          delay: 0.2,
+          onStart() {
+            $('body').addClass('no-scroll')
+            TweenLite.set($('#main-wrapper main,#main-wrapper #main-header,#main-loader'), {paddingRight: S.scrollBarWidth})
+
+          },
+          onComplete() {
+            $('body').removeClass('no-scroll')
+          }
+        })
+
+      TweenLite.set(mainLoader, { autoAlpha: 1 })
+      TweenLite.set(mainLoader.find('.hor-box'), { scaleX: 0.2 })
+      TweenLite.set(mainLoader.find('.ver-box'), { scaleY: 0.2 })
+
+      tl.to(mainLoader.find('.hor-box'), 1, { scaleY: 0.2, ease: Power2.easeInOut })
+        .to(mainLoader.find('.ver-box'), 1, { scaleX: 0.6, ease: Power2.easeInOut }, 0.4)
+        .to(mainLoader.find('.hor-box'), 1, { scale: 0.98, ease: Power2.easeInOut }, 0.6)
+        .to(mainLoader.find('.ver-box'), 1, { scale: 0.98, ease: Power2.easeInOut }, 0.6)
+        .from(mainLoader.find('.logo-holder'), 1, { autoAlpha: 0, scale: 0.5, ease: Power2.easeInOut }, 0.8)
+        .to(mainLoader.find('.hor-box'), 1, { scaleY: 0, ease: Power2.easeInOut }, 2)
+        .to(mainLoader.find('.ver-box'), 1, { scaleX: 0, ease: Power2.easeInOut }, 2)
+        //.to(mainLoader.find('.logo-holder'), 1, { autoAlpha: 0, ease: Power2.easeInOut }, 2)
+        .to(mainLoader, 0.5, { scale: 1.5, autoAlpha: 0, display: 'none', ease: Power2.easeInOut },'ending-seq')
+        .to($('#main-wrapper main,#main-wrapper #main-header,#main-wrapper #main-footer'), 0.5, { scale: 1, autoAlpha: 1, ease: Power2.easeInOut},'ending-seq+=0.3')
+      
+      tl.seek(2)
+      tl.pause()
+    },
+
+    // Scrollchecker
+    // Adding scroll class
+    scrollDierectionChecker() {
+
+      function scrollCheck() {
+        if (window.scrollY > S.scrollClassTrigger) {
+          document.querySelector('body').classList.add('scrolled');
+        } else {
+          document.querySelector('body').classList.remove('scrolled');
+        }
+      }
+
+      function scrollDirCheck(e) {
+        var delta = ((e.deltaY || -e.wheelDelta || e.detail) >> 10) || 1;;
+        if (delta > 0) {
+
+          document.querySelector('body').classList.remove('scrolling-up');
+          document.querySelector('body').classList.add('scrolling-down');
+        } else {
+          document.querySelector('body').classList.remove('scrolling-down');
+          document.querySelector('body').classList.add('scrolling-up');
+        }
+      }
+
+      window.onscroll = function() { scrollCheck() };
+      window.addEventListener('mousewheel', scrollDirCheck);
+      window.addEventListener('DOMMouseScroll', scrollDirCheck);
+
+    },
+
+  }
+
+  Qobj._init()
+
+})(jQuery)
